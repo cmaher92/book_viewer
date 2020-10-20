674049879ffeb4a046e067d6684ed1471f90e744
Connor Maher <connormmaher@gmail.com>
2020-10-19 20:43:23 -0400

    add TOC and chapter 1 data

--- book_viewer.rb
+++ book_viewer.rb
@@ -1,6 +1,18 @@
 require "sinatra"
 require "sinatra/reloader"
+require "tilt/erubis" # used for templating?
 
 get "/" do
-  File.read "public/template.html"
+  # File.read "public/template.html"
+  @title    = "The Adventures of Sherlock Holmes"
+  @contents = File.readlines('data/toc.txt').map(&:chomp)
+  erb :home
+end
+
+get "/chapters/1" do
+  @title    = "Chapter 1"
+  @contents = File.readlines('data/toc.txt')
+  @chapter  = File.read('data/chp1.txt')
+
+  erb :chapter
 end
