97cef055a2ddb97f2a7342949452178bd08c363d
connormmaher <connormmaher@gmail.com>
2020-10-20 21:05:14 -0400

    add before filters

--- book_viewer.rb
+++ book_viewer.rb
@@ -1,18 +1,22 @@
 require "sinatra"
 require "sinatra/reloader"
-require "tilt/erubis" # used for templating?
-require 'pry'
+require "tilt/erubis" # used for templating
+
+before do
+  # code here runs prior to routes making ivars available beforehand
+  @contents = File.readlines('data/toc.txt')
+end
 
 get "/" do
   # File.read "public/template.html"
   @title    = "The Adventures of Sherlock Holmes"
-  @contents = File.readlines('data/toc.txt').map(&:chomp)
   erb :home
 end
 
 get "/chapters/:number" do
-  @title    = "Chapter #{params['number']}"
-  @contents = File.readlines('data/toc.txt')
+  number = params[:number].to_i
+  chapter_name = @contents[number - 1]
+  @title    = "Chapter #{number}: #{chapter_name}"
   @chapter  = File.read("data/chp#{params['number']}.txt")
 
   erb :chapter
